2afcbbbe1cc47d9aad99ca39915d8d80e28e1800
Michael Naumov <mihail.naumov23@gmail.com>
2020-02-12 15:28:30 +0200

    add create and delete recipes

--- app/javascript/components/Recipe.jsx
+++ app/javascript/components/Recipe.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class Recipe extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { recipe: { ingredients: "" } };
-
-    this.addHtmlEntities = this.addHtmlEntities.bind(this);
-  }
+  state = { recipe: { ingredients: "" } };
 
   componentDidMount() {
     const {
@@ -29,10 +24,36 @@ class Recipe extends React.Component {
       .catch(() => this.props.history.push("/recipes"));
   }
 
-  addHtmlEntities(str) {
+  addHtmlEntities = (str) => {
     return String(str)
       .replace(/&lt;/g, "<")
       .replace(/&gt;/g, ">");
+  }
+
+  deleteRecipe = () => {
+    const {
+      match: {
+        params: { id }
+      }
+    } = this.props;
+    const url = `/api/v1/destroy/${id}`;
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(() => this.props.history.push("/recipes"))
+      .catch(error => console.log(error.message));
   }
 
   render() {
@@ -80,7 +101,7 @@ class Recipe extends React.Component {
               />
             </div>
             <div className="col-sm-12 col-lg-2">
-              <button type="button" className="btn btn-danger">
+              <button type="button" className="btn btn-danger" onClick={this.deleteRecipe}>
                 Delete Recipe
               </button>
             </div>
--- app/javascript/components/Recipes.jsx
+++ app/javascript/components/Recipes.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class Recipes extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipes: []
-    };
-  }
+  state = {
+    recipes: []
+  };
 
   componentDidMount() {
     const url = "/api/v1/recipes/index";
